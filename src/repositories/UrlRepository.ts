b283648a408bb5161821ee7c617b0b2043d036b6
tanmayshende007@gmail.com <tanmayshende007@gmail.com>
2024-07-25 16:03:06 +0530

    Repositories are created

---
+++ src/repositories/UrlRepository.ts
@@ -0,0 +1,35 @@
+import Url, {IUrl} from "@/models/Url";
+import connectDB from "@/config/db";
+
+class UrlRepository {
+    private urlModal;
+    constructor() {
+        connectDB();
+        this.urlModal = Url;
+    }
+
+    async getUrlById(id:string): Promise<IUrl | null> {
+        return await this.urlModal.findById(id).lean();
+    }
+
+    async getUrlByShortUrl(shortUrl:string): Promise<IUrl | null> {
+        return await this.urlModal.findOne({shortUrl}).lean();
+    }
+    
+    async getUrlByOriginalUrl(originalUrl:string): Promise<IUrl | null> {
+        return await this.urlModal.findOne({originalUrl}).lean();
+    }
+    
+    async gerAllUrls(): Promise<IUrl | null> {
+        return await this.urlModal.find().lean();
+    }
+
+    async deleteUrl(id:string): Promise<IUrl | null> {
+        return await this.urlModal.findByIdAndDelete(id).lean();
+    }
+
+    async createUrl(shortUrl:string, originalUrl:string): Promise<IUrl> {
+        return await this.urlModal.create({shortUrl, originalUrl});
+    }
+    
+}
